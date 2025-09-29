d65ef1343eebbf7c2e925c1aa07a3511f68b8a7c
Diego Nascimento <diegohfn@gmail.com>
2025-09-29 15:32:33 -0400

    chore: complements the user structure

--- src/main/java/br/com/alura/AluraFake/user/UserController.java
+++ src/main/java/br/com/alura/AluraFake/user/UserController.java
@@ -2,9 +2,14 @@ package br.com.alura.AluraFake.user;
 
 import br.com.alura.AluraFake.util.ErrorItemDTO;
 import jakarta.validation.Valid;
-import org.springframework.http.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,6 +24,7 @@ public class UserController {
 
     @Transactional
     @PostMapping("/user/new")
+    
     public ResponseEntity newStudent(@RequestBody @Valid NewUserDTO newUser) {
         if(userRepository.existsByEmail(newUser.getEmail())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -33,5 +39,4 @@ public class UserController {
     public List<UserListItemDTO> listAllUsers() {
         return userRepository.findAll().stream().map(UserListItemDTO::new).toList();
     }
-
 }
